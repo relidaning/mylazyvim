daeea7d0076b341ce1801be21b752b577c2b9169
relidaning <453882101@qq.com>
2026-03-22 03:32:13 +0800

    comment aerial.

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,9 +1,9 @@
 return {
-  {
-    "stevearc/aerial.nvim",
-    opts = {},
-    keys = {
-      { "<leader>o", "<cmd>AerialToggle!<CR>", desc = "Toggle Outline" },
-    },
-  },
+  -- {
+  --   "stevearc/aerial.nvim",
+  --   opts = {},
+  --   keys = {
+  --     { "<leader>o", "<cmd>AerialToggle!<CR>", desc = "Toggle Outline" },
+  --   },
+  -- },
 }
