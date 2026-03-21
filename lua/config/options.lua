2a2eab36bab3655f5fb4088f88c86b1770e1aee8
relidaning <453882101@qq.com>
2026-03-21 18:11:16 +0800

    aerial: add outline feature.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,5 @@ vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.expandtab = true
+
+vim.opt.clipboard = "unnamedplus"
---
+++ lua/plugins/aerial.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    opts = {},
+    keys = {
+      { "<leader>o", "<cmd>AerialToggle!<CR>", desc = "Toggle Outline" },
+    },
+  },
+}
