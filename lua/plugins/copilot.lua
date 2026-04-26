334d249eced85e6ba9cfece89cada84fbb67a2f3
relidaning <453882101@qq.com>
2026-04-26 23:12:41 +0800

    copilot: enable copilot
    you need enable copilot and overwrite the configuration to use it.

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    filetypes = {
+      yaml = true,
+      markdown = true,
+      lua = true,
+      java = true,
+      python = true,
+      javascript = true,
+      typescript = true,
+    },
+  },
+}
